3cbada747da548459baa4590603172b9320d2988
Adelodun Adeola Mufliah <51162425+bintMA@users.noreply.github.com>
2022-07-30 10:19:28 +0000

    built the html structure.

--- app.mjs
+++ app.mjs
@@ -4,13 +4,15 @@ function startApp() {
     // so create and use/call additional functions from here
   
     // pls remove the below and make some magic in here!
-    console.log('make magic in here!');
   
-    const header = document.querySelector('h2');
-    if(header) {
-      header.textContent = 'make some magic here!!';
-    }
-  };
+    //an array of all the possible prefixes of the different lines
+  //an array of all the possible prefixes of the different lines
+  const mtnPrefixArray = [
+    702, 703, 704, 706, 803, 806, 810, 813, 814, 816, 903, 906, 913, 916,
+  ];
+  const gloPrefixArray = [705, 805, 807, 811, 815, 905, 915];
+  const airtelPrefixArray = [701, 708, 802, 808, 812, 901, 902, 904, 907, 912];
+  const etisalatPrefixArray = [809, 817, 818, 908, 909];
   
   // ======= DO NOT EDIT ============== //
   export default startApp;
